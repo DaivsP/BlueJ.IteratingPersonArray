e994500f4e45f4f5387c1a0fad8e94f573d5940b
Davis Perchik <davisperchik@zipcoders-MacBook-Pro.local>
2019-02-08 17:09:22 -0500

    finished while, for, and for each loops, all tests working

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -12,10 +12,14 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        int counter = 0;
         // assume there is a `counter`
+        while(counter < personArray.length){
         // while `counter` is less than length of array
             // begin loop
-
+                result += personArray[counter].toString();
+                counter++;
+            }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
@@ -31,9 +35,11 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for(int indexCounter  = 0; indexCounter < personArray.length; indexCounter ++){
         // use the above clauses to declare for-loop signature
             // begin loop
+                result += personArray[indexCounter].toString();
+            }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
@@ -48,8 +54,10 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
+        for(Person person : personArray){
+             result += person.toString();
+            }
+            // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
